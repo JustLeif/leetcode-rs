ac2fd323c00c2ccb81688dece7bd238cce320a61
JustLeif <leif@elliottnet.com>
2023-08-20 11:57:08 -0400

    Initializing given Rust code from the leetcode question.

--- src/solutions/problem_2.rs
+++ src/solutions/problem_2.rs
@@ -1,1 +1,44 @@
+/// You are given two non-empty linked lists representing two non-negative integers.
+/// The digits are stored in reverse order, and each of their nodes contains a single digit.
+/// Add the two numbers and return the sum as a linked list.
+///
+/// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+///
+/// Example:
+///
+/// l1 = [2,4,3], l2 = [5,6,4]
+///
+/// 342 + 465 = 807
+///
+/// Output = [7,0,8]
+pub fn solution(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    return None;
+}
 
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+impl ListNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
