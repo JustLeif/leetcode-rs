6828d8178cf27f843f35483f16527b8762fe5c3f
JustLeif <leif@elliottnet.com>
2023-08-22 18:04:29 -0400

    Preparing for problem 2

--- src/solutions/problem_2.rs
+++ src/solutions/problem_2.rs
@@ -11,33 +11,14 @@
 /// 342 + 465 = 807
 ///
 /// Output = [7,0,8]
-pub fn solution(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    return None;
-}
 
 // Definition for singly-linked list.
-// #[derive(PartialEq, Eq, Clone, Debug)]
-// pub struct ListNode {
-//   pub val: i32,
-//   pub next: Option<Box<ListNode>>
-// }
-//
-// impl ListNode {
-//   #[inline]
-//   fn new(val: i32) -> Self {
-//     ListNode {
-//       next: None,
-//       val
-//     }
-//   }
-// }
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
     pub val: i32,
     pub next: Option<Box<ListNode>>,
 }
 impl ListNode {
-    #[inline]
     pub fn new(val: i32) -> Self {
         ListNode { next: None, val }
     }
