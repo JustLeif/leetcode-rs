18fd64ff7934aa1969389073c249c22693ddae94
leif <leif@elliottnet.com>
2023-12-12 07:29:24 -0500

    Problem 5 rust solution.

--- src/solutions/problem_5.rs
+++ src/solutions/problem_5.rs
@@ -1,24 +1,32 @@
 pub fn solution(input: String) -> String {
-    let mut result = &input[0..0];
-    println!("This is result: {}", result);
-    for idx in 0..input.len() {
-        // Loop to check the surrounding char
-        let mut iter_idx: usize = 0;
-        while input.chars().nth(idx + iter_idx) != None
-            && idx - iter_idx > 0
-            && input.chars().nth(idx + iter_idx) == input.chars().nth(idx - iter_idx)
-        {
-            result = &input[idx - iter_idx..idx + iter_idx];
-            println!("this is res: {}", result);
-            iter_idx += 1;
+    let mut left: usize = 0;
+    let mut right: usize = 0;
+
+    let c_vec: Vec<char> = input.chars().collect();
+    for idx in 0..c_vec.len() {
+        let mut tl: i32 = idx as i32 - 1;
+        let mut tr: i32 = idx as i32 + 1;
+
+        while tl >= 0 && (tr as usize) < c_vec.len() && c_vec[tl as usize] == c_vec[tr as usize] {
+            if tr as usize - tl as usize > right - left {
+                left = tl as usize;
+                right = tr as usize;
+            }
+            tl -= 1;
+            tr += 1;
         }
     }
 
-    return String::from(result);
+    return input[left..=right].to_string();
 }
 
 #[cfg(test)]
 #[test]
 fn test_problem5() {
     assert_eq!(solution(String::from("abad")), String::from("aba"));
+    assert_eq!(
+        solution(String::from("aaaaaaaabad")),
+        String::from("aaaaaaa")
+    );
+    assert_eq!(solution(String::from("ftftttf")), String::from("ftttf"));
 }
